ecb01465eb78529a9f2f72e32f7429c5e24e83dd
Htet Lin Maung <htet.lin.maung@fusionsol.com>
2020-04-14 19:27:22 +0630

    push build

---
+++ build/precache-manifest.02bbf1338962c89066f41cfc430b0845.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f70a4a45c0aaec63c31b3d92f80fd33d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c4da8f82f0c09327e9f",
+    "url": "/static/css/main.23dd308e.chunk.css"
+  },
+  {
+    "revision": "d46bf2aab01f73620b22",
+    "url": "/static/js/2.d6fa5eff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d6fa5eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c4da8f82f0c09327e9f",
+    "url": "/static/js/main.8675dd52.chunk.js"
+  },
+  {
+    "revision": "1bb58377db36507569cb",
+    "url": "/static/js/runtime-main.ed00a22d.js"
+  }
+]);
